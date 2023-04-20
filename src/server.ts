10f2350d52f6683a4021683f22cc5d0ffbf015d8
Reinaldo Alves <reinaldoasjr8@gmail.com>
2023-04-20 16:33:10 -0300

    room creation

--- src/server.ts
+++ src/server.ts
@@ -10,22 +10,33 @@ interface IUser {
 }
 
 interface IMessage {
-    user: IUser
-    message: string
-    hour: string
+    user: IUser;
+    message: string;
+    hour: string;
+}
+
+interface IRoom {
+    name: string;
+    avatar: string;
+    users: Array<IUser>;
+    messages: Array<IMessage>;
 }
 
 interface ServerToClientEvents {
     noArg: () => void;
     basicEmit: (a: number, b: string, c: Buffer) => void;
     withAck: (d: string, callback: (e: number) => void) => void;
-    users: (f: Array<IUser>, g: IUser) => void;
-    message: (h: IMessage) => void;
+    users: (f: Array<IUser>) => void;
+    message: (g: IMessage) => void;
+    groupdata: (h: IRoom) => void;
+    rooms: (h: Array<IRoom>) => void;
 }
 
 interface ClientToServerEvents {
     join: (i: string, j: string, k: string) => void;
     message: (l: IMessage) => void;
+    logout: (m: string) => void;
+    newgroup: (n: string, o: string, p: string) => void;
 }
 
 interface SocketData {
@@ -46,23 +57,40 @@ const port = process.env.PORT || 4000;
 // on -> escutando - receptor
 // emit -> enviando algum dado
 
-const users = [] as Array<IUser>;
+let users = [] as Array<IUser>;
 const noUser = {id:'', name:'', avatar: null, color:''};
 
-io.on('connection', (socket) => {
-    socket.on('disconnect', () => {
+const rooms = [] as Array<IRoom>
 
+io.on('connection', (socket) => {
+    socket.on('logout', (name) => {
+        users = users.filter((item) => item.name !== name);
+        io.emit("message", {user: noUser, message: `${name} saiu no chat`, hour: ''})
+        io.emit("users", users)
     })
     
     socket.on("join", (name, avatar, color) => {
         const user = {id: socket.id, name: name, avatar: avatar, color: color};
         users.push(user);
         io.emit("message", {user: noUser, message: `${name} entrou no chat`, hour: ''})
-        io.emit("users", users, user)
+        io.emit("users", users)
+        io.emit("rooms", rooms)
     })
 
     socket.on("message", (message) => {
         io.emit("message", message)
+    })
+
+    socket.on("newgroup", (roomname, avatar, name) => {
+        const user = users.filter((item) => item.name === name);
+        const room = {name: roomname, avatar: avatar, users: user, messages: []}
+        rooms.push(room);
+        socket.join(roomname)
+        io.in(roomname).emit("message", {user: noUser, message: `${name} entrou no grupo`, hour: ''})
+        io.emit("groupdata", room)
+        io.emit("rooms", rooms)
+        console.log('Novo grupo criado', room)
+        console.log('Todos os grupos', rooms)
     })
 })
 
