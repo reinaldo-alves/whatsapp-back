8fff29f1a22dbd46a71a495bdef4009e25c0609f
reinaldo-alves <reinaldoasjr8@gmail.com>
2023-04-23 17:27:47 -0300

    newchat created

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ interface IMessage {
 
 interface IRoom {
     name: string;
-    avatar: string;
+    avatar: string | null;
     users: Array<IUser>;
     messages: Array<IMessage>;
 }
@@ -35,8 +35,9 @@ interface ServerToClientEvents {
 interface ClientToServerEvents {
     join: (i: string, j: string, k: string) => void;
     message: (l: IMessage) => void;
-    logout: (m: string) => void;
+    logout: (m: IUser) => void;
     newgroup: (n: string, o: string, p: string) => void;
+    newchat: (q: IUser, r: IUser) => void;
 }
 
 interface SocketData {
@@ -63,16 +64,16 @@ const noUser = {id:'', name:'', avatar: null, color:''};
 const rooms = [] as Array<IRoom>
 
 io.on('connection', (socket) => {
-    socket.on('logout', (name) => {
-        users = users.filter((item) => item.name !== name);
-        io.emit("message", {user: noUser, message: `${name} saiu no chat`, hour: ''})
+    socket.on('logout', (user) => {
+        users = users.filter((item) => item.id !== user.id);
+        io.emit("message", {user: noUser, message: `${user.name} saiu no chat`, hour: ''})
         io.emit("users", users)
     })
     
     socket.on("join", (name, avatar, color) => {
         const user = {id: socket.id, name: name, avatar: avatar, color: color};
         users.push(user);
-        io.emit("message", {user: noUser, message: `${name} entrou no chat`, hour: ''})
+        socket.broadcast.emit("message", {user: noUser, message: `${name} entrou no chat`, hour: ''})
         io.emit("users", users)
         io.emit("rooms", rooms)
     })
@@ -87,6 +88,19 @@ io.on('connection', (socket) => {
         rooms.push(room);
         socket.join(roomname)
         io.in(roomname).emit("message", {user: noUser, message: `${name} entrou no grupo`, hour: ''})
+        io.emit("groupdata", room)
+        io.emit("rooms", rooms)
+        console.log('Novo grupo criado', room)
+        console.log('Todos os grupos', rooms)
+    })
+
+    socket.on("newchat", (otherUser, user) => {
+        const userschat = [user, otherUser]
+        const chatName = userschat.filter((item) => item.id !== user.id)
+        const room = {name: chatName[0].name, avatar: chatName[0].avatar, users: userschat, messages: []}
+        rooms.push(room);
+        const roomName = user.id.concat(otherUser.id);
+        socket.join(roomName)
         io.emit("groupdata", room)
         io.emit("rooms", rooms)
         console.log('Novo grupo criado', room)
